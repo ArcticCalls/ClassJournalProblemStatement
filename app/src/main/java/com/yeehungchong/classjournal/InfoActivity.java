e7919065c8b38344a500b6dd6907c56f23b57c0f
Yee Hung <19034609@myrp.edy.sg>
2021-04-29 16:31:30 +0800

    Update

--- app/src/main/java/com/yeehungchong/classjournal/InfoActivity.java
+++ app/src/main/java/com/yeehungchong/classjournal/InfoActivity.java
@@ -10,6 +10,7 @@ import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +21,8 @@ public class InfoActivity extends AppCompatActivity {
     ArrayAdapter aa;
     ArrayList<GradeInfo> grade;
     String emailMessage = "";
+
+    int requestAdd = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +47,10 @@ public class InfoActivity extends AppCompatActivity {
             @Override
             //Qayyum Codes
             public void onClick(View v) {
-                Intent i = new Intent(InfoActivity.this,
-                        AddActivity.class);
-
+                Intent i = new Intent(InfoActivity.this, AddActivity.class);
+                i.putExtra("currentWeek", grade.size());
                 Module module = (Module) i.getSerializableExtra("module");
-
-                startActivity(i);
-
+                startActivityForResult(i, requestAdd);
             }
         });
 
@@ -61,7 +61,6 @@ public class InfoActivity extends AppCompatActivity {
                 Intent infoIntent = new Intent(Intent.ACTION_VIEW);
                 infoIntent.setData(Uri.parse("https://www.rp.edu.sg/SOI/full-time-diplomas/Details/diploma-in-digital-design-and-development"));
                 startActivity(infoIntent);
-
             }
         });
 
@@ -70,20 +69,34 @@ public class InfoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Jun Yang Codes
                 Intent email = new Intent(Intent.ACTION_SEND);
-
                 email.putExtra(Intent.EXTRA_EMAIL, new String[]{"jason_lim@rp.edu.sg"});
                 email.putExtra(Intent.EXTRA_SUBJECT, "Test Email from C347");
                 for (int i =0; i <grade.size(); i++){
                    emailMessage += "Week " + grade.get(i).getWeek() + ": "+ grade.get(i).getTitle() + ": " + grade.get(i).getGrade() + "\n";
                 }
                 email.putExtra(Intent.EXTRA_TEXT, "Hi Faci, \n" + "I am..." + "\n Please see my remarks so far, thank you! \n" + emailMessage  );
-
                 email.setType("message/rfc822");
-
                 startActivity(Intent.createChooser(email, "Choose an Email client :"));
-
             }
         });
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if (data != null) {
+                String grade = data.getStringExtra("grade");
+                int week = data.getIntExtra("week", 0);
+                String statement = "";
+                if(requestCode == requestAdd){
+                    statement = "Grade " + grade + " week " + week;
+                }
+                grade.add(new GradeInfo(week, "DG", grade));
+                aa.notifyDataSetChanged();
+            }
+        }
+    }
+
 }
